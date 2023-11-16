6b3cdcf287edee1232cea8c0b21a2ee2a649eef1
[Josh Nguyen] <[joshnguyentoronto@gmail.com]>
2023-11-16 00:39:46 -0500

    complete homepage layout

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/css", express.static("dist"));
 
 app.use('/', require('./routes/home'));
 
