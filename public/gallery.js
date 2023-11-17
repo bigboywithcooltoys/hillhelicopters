abf0c4b56b21708fbab06325e364b1ac0ffa8198
[Josh Nguyen] <[joshnguyentoronto@gmail.com]>
2023-11-17 16:44:23 -0500

    complete most js operation

--- public/gallery.js
+++ public/gallery.js
@@ -1,3 +1,7 @@
+
+
+
+
 let videos = [
     "https://www.youtube.com/embed/5pcS2UG1dlY?start=0&modestbranding=1&rel=0&cc_load_policy=1&iv_load_policy=3",
     "https://www.youtube.com/embed/zoi95c63d0I?start=0&modestbranding=1&rel=0&cc_load_policy=1&iv_load_policy=3",
@@ -8,11 +12,22 @@ let videos = [
 
 
 let vids = document.querySelectorAll(".g-2-c-cover")
+let vid = document.getElementById("g-1-m-i-vid")
 
 
 
 function playVid(e) {
-    console.log("hello")
+    if (e.target.id == "vid-1") {
+        vid.src=videos[0]
+    } else if (e.target.id == "vid-2") {
+        vid.src=videos[1]
+    } else if (e.target.id == "vid-3") {
+        vid.src=videos[2]
+    } else if (e.target.id == "vid-4") {
+        vid.src=videos[3]
+    }
+    document.getElementById("g-1-toggle").classList.add("g-1-active")
+    document.getElementById("g-1-m-toggle").classList.add("g-1-m-active")
 }
 
 
